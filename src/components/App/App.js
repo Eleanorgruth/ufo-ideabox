47717aec15824e87fe48e0a5814c251404939034
Eleanor Ruth <108287127+Eleanorgruth@users.noreply.github.com>
2023-01-02 06:53:17 -0700

    Revert "Add comment on useEffect"

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,35 +7,31 @@ import Form from "../Form/Form"
 const App = () => {
   const [data, changeData] = useState([])
 
-  const addIdea = async (newIdea) => {
-    try {
-      const response = await postData(newIdea)
-      changeData([...data, response])
-    }
-    catch (error) {
-      console.log(error)
-    }
+ const addIdea = (newIdea) => {
+    postData(newIdea)
+    .then(response => changeData([...data, response]))
+    .catch(error => console.log(error))
   }
-  const deleteIdea = async (id) => {
-    const response = await deleteData(id)
-    changeData(response)
+
+  const deleteIdea = (id) => {
+    deleteData(id)
+    .then(response => changeData(response))
   }
-  const test = async () => {
-    const response = await getData()
-    changeData(response)
+  const test = () => {
+    getData()
+      .then(response => changeData(response))
 
   }
-  //UseEffect can only return a clean-up function or nothing at all, so we havew to write the test() function outside of it and call it inside 
-  useEffect(() => {
-    test()
-  }, [])
+useEffect(()=> {
+  test()
+}, [])
 
-  return (
-    <main>
-      <h1>UFO Sightings</h1>
-      <Form addIdea={addIdea} />
-      <IdeaContainer data={data} deleteIdea={deleteIdea} />
-    </main>
-  )
+    return (
+      <main>
+        <h1>UFO Sightings</h1>
+        <Form addIdea={addIdea}/>
+        <IdeaContainer data={data} deleteIdea={deleteIdea} />
+      </main>
+    )
 }
 export default App;
